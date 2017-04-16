79a55974e7c73cde3490c2523f6d5fc9e71b4996
Sarthak Khanna <khannasarthak.1994@gmail.com>
2017-04-16 01:36:41 -0700

    added more queries
    left wioth alst 2 queries and user

--- Flask App/app.py
+++ Flask App/app.py
@@ -51,6 +51,102 @@ def pq1():
     mysql.connection.commit()
     return render_template('q1d.html', query = query)
 
+@app.route('/q2', methods=['POST']) # Option for admin to add event
+
+def pq2():
+    showname = request.form['query']
+    showname = '"%'+showname+'%"'
+    query = 'SELECT show_name, COUNT(reservation.show_id) AS num_of_reservations FROM	shows JOIN 	reservation ON reservation.show_id = shows.show_id  WHERE show_name like '+showname+' GROUP BY reservation.show_id '
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q2.html', dbstuff = data, columns= colNames, query = query)
+
+@app.route('/q4', methods=['POST']) # Number of movies released in the year selected by the admin
+def pq4():
+    year = request.form['year']
+    print (year)
+    query = 'SELECT release_date, COUNT(shows.show_id) AS Total_movie FROM shows JOIN movie ON movie.show_id = shows.show_id WHERE release_date ='+year+' GROUP BY release_date'
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q4.html', dbstuff = data, columns= colNames, query = query)
+
+@app.route('/q5', methods=['POST']) # viewtop 10
+def pq5():
+    showname = request.form['query']
+    showname = '"%'+showname+'%"'
+    query = 'SELECT e_id, ticket_price FROM eventtable WHERE 	eventtable.show_id in (SELECT  shows.show_id FROM shows	WHERE 	show_name like '+showname+')'
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q5.html', dbstuff = data, columns= colNames, query = query)
+
+@app.route('/q6', methods=['POST']) # viewtop 10
+def pq6():
+    ptype = request.form['query']
+    ptype = '"%'+ptype+'%"'
+    num = request.form['num']
+    query = 'SELECT	performance.show_id, performers, show_name FROM	performance, shows WHERE	performance_type like '+ptype+' 	AND shows.show_id = performance.show_id LIMIT '+ num
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q6.html', dbstuff = data, columns= colNames, query = query)
+
+@app.route('/q7', methods=['POST']) # viewtop 10
+def pq7():
+    eid = request.form['query']
+    eid = '"'+eid+'"'
+
+    query = 'SELECT	COUNT(booking_id) FROM	booking WHERE 	e_id = '+ eid+ ' GROUP BY e_id'
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q7.html', dbstuff = data, columns= colNames, query = query)
+
+@app.route('/q8', methods=['POST']) # viewtop 10
+def pq8():
+    numt = request.form['query']
+    numt = '"'+numt+'"'
+    num = request.form['num']
+    query = 'SELECT customer_name FROM	(SELECT    customer_id, COUNT(booking.num_tickets) 	FROM    	booking	GROUP BY booking.customer_id	HAVING COUNT(booking.num_tickets) = '+numt+ ') AS s,	customer WHERE 	s.customer_id = customer.customer_id LIMIT '+num
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q8.html', dbstuff = data, columns= colNames, query = query)
+
+@app.route('/q9', methods=['POST']) # viewtop 10
+def pq9():
+    eid = request.form['query']
+    eid = '"'+eid+'"'
+
+    query = 'SELECT	hall.name_hall FROM	hall WHERE hall.hall_id IN (SELECT reservation.hall_id FROM reservation WHERE reservation.show_id IN (SELECT show_id FROM eventtable WHERE e_id ='+eid+'))'
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q9.html', dbstuff = data, columns= colNames, query = query)
+
+@app.route('/q10', methods=['POST']) # viewtop 10
+def pq10():
+    showid = request.form['query']
+    showid = '"'+showid+'"'
+
+    query = 'SELECT	show_name, reservation.r_date, reservation.r_time FROM reservation JOIN shows ON reservation.show_id = shows.show_id WHERE	reservation.show_id = '+showid
+    cursor = mysql.connection.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    colNames = [i[0] for i in cursor.description]
+    return render_template('q10.html', dbstuff = data, columns= colNames, query = query)
+
+
+
 
 @app.route('/execute', methods = ['POST','GET'])
 def execute():
@@ -83,6 +179,22 @@ def q6():
 @app.route('/execute/q7', methods = ['POST','GET'])
 def q7():
     return render_template('q7.html')
+
+@app.route('/execute/q9', methods = ['POST','GET'])
+def q9():
+    return render_template('q9.html')
+
+@app.route('/execute/q10', methods = ['POST','GET'])
+def q10():
+    return render_template('q10.html')
+
+@app.route('/execute/q11', methods = ['POST','GET'])
+def q11():
+    return render_template('q11.html')
+
+@app.route('/execute/q12', methods = ['POST','GET'])
+def q12():
+    return render_template('q12.html')
 
 @app.route('/execute/q8', methods = ['POST','GET'])
 def q8():
